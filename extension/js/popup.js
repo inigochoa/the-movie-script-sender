57f903a503a30ab469ed94af34855350e342515b
Iñigo Ochoa <contact@inigochoa.me>
2024-06-22 14:19:56 +0200

    feat(popup): inject function directly

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -2,6 +2,49 @@ document.querySelectorAll('[data-i18n]').forEach(elem => {
   elem.innerText = chrome.i18n.getMessage(elem.dataset.i18n)
 })
 
+const mssSendLines = () => {
+  const html = document.querySelector('html#whatsapp-web')
+  if (!html) {
+    return
+  }
+
+  const main = document.querySelector('#main')
+  if (!main) {
+    return
+  }
+
+  const textarea = main.querySelector(`div[contenteditable="true"]`)
+  if (!textarea) {
+    return
+  }
+
+  const findLanguage = () => {
+    const availables = ['en', 'es']
+
+    for (const lang of navigator.languages) {
+      if (availables.includes(lang.substring(0, 2))) {
+        return lang.substring(0, 2)
+      }
+    }
+
+    return 'en'
+  }
+
+  fetch(`https://raw.githubusercontent.com/inigochoa/the-movie-script-sender/main/scripts/shrek/${findLanguage()}.json`)
+    .then((response) => response.json())
+    .then(async (lines) => {
+      for (const line of lines) {
+        textarea.focus()
+        document.execCommand('insertText', false, line)
+        textarea.dispatchEvent(new Event('change', { bubbles: true }))
+
+        setTimeout(() => (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click(), 100)
+
+        await new Promise(resolve => setTimeout(resolve, 500))
+      }
+    })
+}
+
 const mssOnClick = () => {
   button.disabled = true
 
@@ -12,7 +55,7 @@ const mssOnClick = () => {
   tabs => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      files: ['js/content_script.js']
+      func: mssSendLines,
     })
   })
 }
